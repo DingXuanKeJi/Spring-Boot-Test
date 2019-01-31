61dfe0eaa1c4dccad6560bbb169e06c858832296
kaileiweike <894693839@qq.com>
2019-01-31 17:37:51 +0800

    测试

--- wx/src/main/java/com/wx/java/job/SchedulingMission.java
+++ wx/src/main/java/com/wx/java/job/SchedulingMission.java
@@ -14,19 +14,19 @@ public class SchedulingMission {
 	
 	private static final String format = "yyyy-MM-dd hh:mm:ss";
 
-//	/**
-//	 * 2秒的
-//	 */
-//	@Scheduled(cron = "0/2 * * * * *")
-//	public void test() {
-//		System.out.println("我是定时器1,现在是"+LocalDateTime.now().format(DateTimeFormatter.ofPattern(format)));
-//	}
-//	
-//	/**
-//	 * 5秒的
-//	 */
-//	@Scheduled(fixedRate = 5000)
-//	public void testtwo() {
-//		System.out.println("我是定时器2，现在是"+LocalDateTime.now().format(DateTimeFormatter.ofPattern(format)));
-//	}
+	/**
+	 * 2秒的
+	 */
+	@Scheduled(cron = "0/2 * * * * *")
+	public void test() {
+		System.out.println("我是定时器1,现在是"+LocalDateTime.now().format(DateTimeFormatter.ofPattern(format)));
+	}
+	
+	/**
+	 * 5秒的
+	 */
+	@Scheduled(fixedRate = 5000)
+	public void testtwo() {
+		System.out.println("我是定时器2，现在是"+LocalDateTime.now().format(DateTimeFormatter.ofPattern(format)));
+	}
 }
